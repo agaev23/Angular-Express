9b414d963b506343b8219e6d01ee16525a062899
Ihor_Ahaiev <Ihor_Ahaiev@epam.com>
2019-07-03 14:27:29 +0300

    added categories page

--- controllers/category.js
+++ controllers/category.js
@@ -2,9 +2,10 @@ const Category = require('../models/Category');
 const Position = require('../models/Position');
 const errorHandler = require('../utils/errorHandler');
 
-
 module.exports.getAll = async function (req, res) {
     try {
+
+        console.log('---------- ',req.params.id);
         const categories = await Category.find({user: req.user.id});
         res.status(200).json(categories)
     } catch (e) {
@@ -12,9 +13,12 @@ module.exports.getAll = async function (req, res) {
     }
 };
 
-module.exports.getById = async function (req, res) {
+module.exports.getById = async function(req, res) {
+
+    console.log('---------- ',req.params.id);
     try {
-        const category = await Category.findById({_id: req.params.id});
+        console.log('---------- ',req.params.id);
+        const category = await Category.findById(req.params.id);
         res.status(200).json(category)
     } catch (e) {
         errorHandler(res, e)
@@ -39,6 +43,7 @@ module.exports.create = async function (req, res) {
         user: req.user.id,
         imageSrc: req.file ? req.file.path : ''
     });
+
     try {
         await category.save();
         res.status(201).json(category)
